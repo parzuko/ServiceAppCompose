3f4774140a1b13ad21d716c4e43659871b224052
parzuko <jivansh17@gmail.com>
2020-10-17 21:47:59 +0530

    start project

---
+++ app/src/main/java/com/jivansh/serviceapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.jivansh.serviceapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.jivansh.serviceapp.ui.ServiceAppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ServiceAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ServiceAppTheme {
+        Greeting("Android")
+    }
+}
