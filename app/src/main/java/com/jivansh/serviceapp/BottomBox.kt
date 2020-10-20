ce3e1c44c20130f4688a51c206ad8c06af52d30b
parzuko <jivansh17@gmail.com>
2020-10-20 14:04:18 +0530

    finishing touches

--- app/src/main/java/com/jivansh/serviceapp/BottomBox.kt
+++ app/src/main/java/com/jivansh/serviceapp/BottomBox.kt
@@ -22,12 +22,14 @@ import com.jivansh.serviceapp.ui.shade
 
 @Composable
 fun BottomBox(){
-    ScrollableColumn() {
-        PersonInfo(workerName = "Marcus Rashford",facility = "Goals.Goals.Goals", id = R.drawable.human, time = "6:00 PM")
-        PersonInfo(workerName = "Anthony Martial",facility = "RB + O. Finesse", id = R.drawable.bro, time = "7:00 AM")
-        PersonInfo(workerName = "Paul Pogba",facility = "Fancy FrenchMen", id = R.drawable.bro, time = "10:00 PM")
-        PersonInfo(workerName = "Aaron Wan Bisaka",facility = "Running Wall.", id = R.drawable.human, time = "9:00 AM")
-        PersonInfo(workerName = "Bruno Fernandes",facility = "Leader", id = R.drawable.bro, time = "11:00 PM")
+    Card(backgroundColor = Color.White) {
+        ScrollableColumn() {
+            PersonInfo(workerName = "Marcus Rashford",facility = "Goals.Goals.Goals", id = R.drawable.human, time = "6:00 PM")
+            PersonInfo(workerName = "Anthony Martial",facility = "RB + O. Finesse", id = R.drawable.bro, time = "7:00 AM")
+            PersonInfo(workerName = "Paul Pogba",facility = "Fancy FrenchMen", id = R.drawable.bro, time = "10:00 PM")
+            PersonInfo(workerName = "Aaron Wan Bisaka",facility = "Running Wall.", id = R.drawable.human, time = "9:00 AM")
+            PersonInfo(workerName = "Bruno Fernandes",facility = "Leader", id = R.drawable.bro, time = "11:00 PM")
+        }
     }
 }
 
